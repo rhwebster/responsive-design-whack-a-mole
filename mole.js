79c1beed03c4bbbfa9a7eb329028a37d92c241f5
Ryan Webster <ryan.h.webster@gmail.com>
2020-11-04 15:05:31 -0800

    trying to get the moles to hide

--- mole.js
+++ mole.js
@@ -1,1 +1,31 @@
 // Write your JS here.
+window.addEventListener('DOMContentLoaded', () => {
+
+    addEventListener('click', () => {
+        setTimeout(() => {
+            popUpRandomMole();
+        }, 0);
+    });
+    
+    function popUpRandomMole() {
+        let moles = document.querySelectorAll('.wgs__mole-head');
+        let index = Math.floor(Math.random() * Math.floor(moles.length));
+        let mole = moles[index];
+
+        mole.classList.remove('wgs__mole-head--hidden');
+
+        setTimeout(() => {
+            hideMole();
+        }, 1000);
+    }
+
+    function hideMole() {
+        mole.classList.add('wgs__mole-head--hidden')
+        setTimeout(() => {
+            popUpRandomMole();
+         }, 1000)
+    }
+
+    hideMole();
+
+});
